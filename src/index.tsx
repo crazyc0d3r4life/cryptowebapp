8643f24fa5d01e957dac038811fb9c8270684cf0
Promise CrazyC0der <crazyc0d3r4life@gmail.com>
2023-04-06 05:57:13 -0700

    Just addemd

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
 
+import "./assets/sass/main.scss";
+
 const container = document.getElementById('root')!;
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <h1>Hello from Promise</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt nulla esse facilis, ex, explicabo eaque porro iure quod placeat et, exercitationem rerum sapiente illo! Unde reiciendis dolore consequuntur obcaecati veniam.</p>
   </React.StrictMode>
 );
 
